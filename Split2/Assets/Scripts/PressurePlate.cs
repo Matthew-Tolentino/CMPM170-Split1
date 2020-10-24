c0c43a32beaedaec0b2f27b1cf34a837a4a7c5ec
Matthew <lemtolen@ucsc.edu>
2020-10-23 23:53:03 -0700

    Added in pressure plate with timer

---
+++ Split2/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public BoxCollider doorCollider = null;
+
+    public float closeTime = 3.0f;
+
+    private bool onPressurePlate;
+
+    void OnValidate()
+    {
+        Initalize();
+    }
+
+    // Create Door Object that connects to pressure plate
+    void Initalize()
+    {
+        if (doorCollider == null)
+        {
+            Debug.Log("Initalize");
+            GameObject door = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            door.name = "Door";
+            door.transform.parent = transform;
+
+            doorCollider = door.GetComponent<BoxCollider>();
+        }
+    }
+
+
+    void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log("Collided");
+        if (obj.CompareTag("Player"))
+        {
+            OpenDoor();
+            onPressurePlate = true;
+        }
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        Debug.Log("Exit");
+        if (obj.CompareTag("Player"))
+        {
+            onPressurePlate = false;
+            StartCoroutine(CloseDoorAfterTime(closeTime));   
+        }
+    }
+
+    private void OpenDoor()
+    {
+        doorCollider.enabled = false;
+    }
+
+    IEnumerator CloseDoorAfterTime(float time)
+    {
+        if (onPressurePlate) yield break;
+        float timer = 0;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        
+
+        Debug.Log("Done with Coroutine");
+        doorCollider.enabled = true;
+
+
+    }
+}
