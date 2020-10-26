a365fcab2522994926a37928984f78cbbeee6973
Matthew <lemtolen@ucsc.edu>
2020-10-26 16:17:15 -0700

    Changed how pressure plate timer works for more simplicity

--- Split2/Assets/Scripts/DoorController.cs
+++ Split2/Assets/Scripts/DoorController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    private BoxCollider doorCollider;
+
+    private float doorTimer;
+
+    private float closingTime;
+
+    private bool closingDoor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorCollider = GetComponent<BoxCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check to see if player is off the plate and if enough time elapsed to close the door
+        //Debug.Log(Time.time > closingTime);
+        if (closingDoor && Time.time > closingTime)
+        {
+            doorCollider.enabled = true;
+        }
     }
+
+    public void OpenDoor()
+    {
+        closingDoor = false;
+        doorCollider.enabled = false;
+    }
+
+    public void CloseDoor(float timer)
+    {
+        closingDoor = true;
+        doorTimer = timer;
+        closingTime = doorTimer + Time.time;
+    }   
 }
---
+++ Split2/Assets/Scripts/SimplePlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
+}
