48adb2e9d787d39cf5dedd5e973b302a0d40e6b1
tryncalibur <tryncalibur@gmail.com>
2020-10-27 21:21:34 -0700

    Added way to pick up floating spirits. In progress of being able to drop spirits from human vision

---
+++ Split2/Assets/Scripts/Spirit-Player Relationship/SpiritHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritHandler : MonoBehaviour
+{
+    public static float rotDegree;
+    public float rotSpeed;
+
+    private GameObject[] floatingSpirits;
+
+    void Start()
+    {
+        rotDegree = 0f;
+    }
+
+
+    void Update()
+    {
+        rotDegree += rotSpeed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Spirit_Floating")
+        {
+            var pull = collision.gameObject.GetComponent<SpiritMovement_Floating>();
+            pull.ObtainSpiritFloating();
+        }
+    }
+
+}
---
+++ Split2/Assets/Scripts/Spirit-Player Relationship/SpiritMovement_Floating.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritMovement_Floating : MonoBehaviour
+{
+    public Transform player;
+    public float radius;
+    public float initialD;
+
+    public float verticalFluct;
+    public float initialF;
+    public float numVertFluct;
+
+    private Vector3 moveTo;
+    private Rigidbody rb;
+    private string state;
+
+    public Vector3 spawn;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        state = "Spawn";
+    }
+
+    private void Update()
+    {
+        if (state == "OnPlayer")
+        {
+            float angle = SpiritHandler.rotDegree * Time.fixedDeltaTime;
+            moveTo = player.transform.position + new Vector3((radius * Mathf.Cos(angle + initialD)), 0f, (radius * Mathf.Sin(angle + initialD)));
+            moveTo.y += Mathf.Cos(angle * numVertFluct + initialF) * verticalFluct;
+        }
+        else if (state == "ReturningToSpawn")
+        {
+
+            if (transform.position == spawn)
+            {
+                Collider fs = GetComponent<Collider>();
+                fs.enabled = true;
+                state ="Spawn";
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (state != "Spawn")
+            rb.MovePosition(moveTo);
+    }
+
+    public void ObtainSpiritFloating()
+    {
+        Collider fs = GetComponent<Collider>();
+        fs.enabled = false;
+        state = "OnPlayer";
+    }
+
+    public void ReleaseSpiritFloating()
+    {
+        state = "ReturningToSpawn";
+    }
+}
