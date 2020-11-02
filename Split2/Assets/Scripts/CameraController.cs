e4c5aa5db57753a2defc76c09d38fa298dcf80a9
Matthew <lemtolen@ucsc.edu>
2020-11-01 19:43:47 -0800

    Added in cinamachine and 3rd person camera

---
+++ Split2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    public float mouseSensitivity = 100.0f;
+    public float maxLookUp = 90.0f;
+    public float minLookDown = -90.0f;
+
+    float lookUpRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        lookUpRotation -= mouseY;
+        lookUpRotation = Mathf.Clamp(lookUpRotation, minLookDown, maxLookUp);
+
+        transform.localRotation = Quaternion.Euler(lookUpRotation, 0.0f, 0.0f);
+        Player.Rotate(Vector3.up * mouseX);
+
+    }
+}
--- Split2/Assets/Scripts/SimplePlayerMovement.cs
+++ Split2/Assets/Scripts/SimplePlayerMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class SimplePlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-
     private Rigidbody rb;
+
+    public float acceleration = 5f;
+
+    public float maxMoveSpeed = 20;
+
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +21,16 @@ public class SimplePlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.AddForce(Vector3.forward * speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(Vector3.left * speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(Vector3.back * speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(Vector3.right * speed);
-        }
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * acceleration * vertical);
+        rb.AddForce(transform.right * acceleration * horizontal);
+
+        if (rb.velocity.magnitude > maxMoveSpeed)
+            rb.velocity = rb.velocity.normalized * maxMoveSpeed;
     }
 }
