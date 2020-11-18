abeaa984b7cac5cdf70773b5274103ccbc25c393
tryncalibur <tryncalibur@gmail.com>
2020-11-17 18:21:00 -0800

    Added both spirit type scripts to split 3. Need more info on placement and possible collider issues

---
+++ lAmP(Split3)/Assets/Scripts/Spirits/SpiritHandler.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritHandler : MonoBehaviour
+{
+    public static float rotDegree;
+    public float rotSpeed;
+
+    public GameObject[] SpiritList;
+
+    public bool ability;
+
+    void Start()
+    {
+        rotDegree = 0f;
+        ability = false;
+    }
+
+
+    void Update()
+    {
+        rotDegree += rotSpeed * Time.fixedDeltaTime;
+        //Tester
+        //------------------------------------------------------------------
+        if (Input.GetKeyDown("space")) loseSpirit();
+
+        if (Input.GetKeyDown("z")) callAbiliy();
+        //------------------------------------------------------------------
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Spirit_Floating")
+        {
+            var pull = collision.gameObject.GetComponent<SpiritMovement_Floating>();
+            if (pull.state != "Spawn") return;
+            pull.ObtainSpiritFloating();
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>(), true);
+        }
+        else if (collision.gameObject.tag == "Spirit_Land")
+        {
+            var pull = collision.gameObject.GetComponent<SpriritMovement_Land>();
+            if (pull.state != "Spawn") return;
+            pull.ObtainSpiritLand();
+        }
+        else return;
+
+        for (int i = 0; i < 6; ++i)
+        {
+            if (SpiritList[i] == null)
+            {
+                SpiritList[i] = collision.gameObject;
+                break;
+            }
+        }
+    
+
+
+    }
+
+    private void loseSpirit()
+    {
+        int i = 0;
+        while (i < 6)
+        {
+            if (SpiritList[i] == null)
+                ++i;
+            else
+            {
+                if (SpiritList[i].tag == "Spirit_Floating")
+                {
+                    var pull = SpiritList[i].GetComponent<SpiritMovement_Floating>();
+                    pull.ReleaseSpiritFloating();
+                    Physics.IgnoreCollision(SpiritList[i].GetComponent<Collider>(), GetComponent<Collider>(), true);
+                }
+                else if (SpiritList[i].tag == "Spirit_Land")
+                {
+                    var pull = SpiritList[i].GetComponent<SpriritMovement_Land>();
+                    pull.ReleaseSpiritLand();
+                }
+                SpiritList[i] = null;
+                return;
+            }
+        }
+    }
+
+    private void callAbiliy()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            if (SpiritList[i].tag == "Spirit_Land")
+            {
+                var pull = SpiritList[i].GetComponent<SpriritMovement_Land>();
+                if (!ability)
+                {
+                    pull.abilityMove(new Vector3(0.0f, 1.0f, -4.0f));
+                    ability = true;
+                }
+                else pull.ObtainSpiritLand();
+                return;
+            }
+        }
+    }
+
+}
---
+++ lAmP(Split3)/Assets/Scripts/Spirits/SpiritMovement_Floating.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritMovement_Floating : MonoBehaviour
+{
+    public Transform player;
+    public float radius;
+    public float initialD;
+
+    public float verticalFluct;
+    public float initialF;
+    public float numVertFluct;
+
+    private Vector3 moveTo;
+    private Rigidbody rb;
+    public string state;
+
+    private Vector3 spawn;
+    public float speed;
+
+    public string type;
+
+    private void Start()
+    {
+        spawn = transform.position;
+        rb = GetComponent<Rigidbody>();
+        state = "Spawn";
+        if (type == "") type = "NULL";
+    }
+
+    private void Update()
+    {
+        if (state == "OnPlayer")
+        {
+            float angle = SpiritHandler.rotDegree * Time.fixedDeltaTime;
+            Vector3 raiseHeight = player.transform.position;
+            raiseHeight.y += 2;
+            moveTo = raiseHeight + new Vector3((radius * Mathf.Cos(angle + initialD)), 0f, (radius * Mathf.Sin(angle + initialD)));
+            moveTo.y += Mathf.Cos(angle * numVertFluct + initialF) * verticalFluct;
+        }
+        else if (state == "ReturningToSpawn")
+        {
+            moveTo = spawn;
+            if (transform.position == spawn)
+            {
+                Collider fs = GetComponent<Collider>();
+                fs.enabled = true;
+                state ="Spawn";
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (state != "Spawn")
+        {
+            Vector3 direction = (moveTo - transform.position).normalized * (moveTo - transform.position).magnitude * speed;
+            rb.velocity = direction;
+        }
+            
+    }
+
+    public void ObtainSpiritFloating()
+    {   
+        state = "OnPlayer";
+    }
+
+    public void ReleaseSpiritFloating()
+    {
+        Collider fs = GetComponent<Collider>();
+        fs.enabled = false;
+        state = "ReturningToSpawn";
+    }
+
+}
