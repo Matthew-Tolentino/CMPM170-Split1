0d93c3cf5a150fca40523ca60778ea5cc2298406
JDamienDuncan <JDamienDuncan@gmail.com>
2021-01-13 06:25:43 -0800

    John Update: Added Patrolling Enemy and Placed In Vulpecula Model

---
+++ lAmP(Split3)/Assets/John's Stuff/scripts/Enemy Scripts/Patroller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour
+{
+
+    public Transform[] waypoints;
+    public int speed;
+
+    private int waypointIndex;
+    private float dist;
+
+    
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+
+    }
+
+    
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        Debug.Log("This "+dist);
+        if(dist < 1f)
+        {
+            
+            IncreaseIndex();
+            UnityEngine.Debug.Log(waypointIndex);
+        }
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    
+}
