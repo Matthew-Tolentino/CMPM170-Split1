cf40545c37d880d2a97c88062aff14abb174f494
Matthew <lemtolen@ucsc.edu>
2021-01-13 16:49:40 -0800

    Implemented Main Menu

---
+++ lAmP(Split3)/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Henry");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
