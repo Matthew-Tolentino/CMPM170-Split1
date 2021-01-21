4c4d0d3afdbe6e22e701cbe00978c03f4cd10257
Trystan Nguyen <tryncalibur@gmail.com>
2021-01-21 00:49:38 -0800

    Finished Reveal shader for maze

---
+++ URP_Testing/Assets/VFX_Shaders/RevealShaderScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RevealShaderScript : MonoBehaviour
+{
+	public Transform m_objectToTrack = null;
+	private Material m_materialRef = null;
+	private Renderer m_renderer = null;
+    
+
+
+
+    public Renderer Renderer{
+    	get{
+    		if(m_renderer == null)
+    			m_renderer = this.GetComponent<Renderer>();
+			return m_renderer;
+    	}
+    }
+
+    public Material MaterialRef
+    {
+    	get
+    	{
+    		if(m_materialRef == null)
+    			m_materialRef = Renderer.material;
+			return m_materialRef;
+    	}
+    }
+
+	private void Awake(){
+    	m_renderer = this.GetComponent<Renderer>();
+    	m_materialRef = m_renderer.material;
+    }
+
+    private void Update()
+    {
+    	if (m_objectToTrack != null)
+    	{
+    		Debug.Log(m_objectToTrack);
+    		MaterialRef.SetVector("_Vector3_Position", m_objectToTrack.position);
+    	}
+    }
+
+    private void OnDestory(){
+    	m_renderer = null;
+    	if (m_materialRef != null)
+    		Destroy(m_materialRef);
+		m_materialRef = null;
+    }
+
+}
