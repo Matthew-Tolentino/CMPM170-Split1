20e1dbca27f72102042e162e5e195c7e26b38fba
Niccolo Abate <lofieloomis@gmail.com>
2021-02-05 20:51:25 -0800

    Implemented Mixer Controller to control levels, organized audio in unity
    Added MixerController script
    Added Audio objects in main menu and level 1 that have the MixerComponent script as well as most of the audio inside of it as child objects.
    The important exceptions to this is when a sound is tied to a specific object that is placed elsewhere (like the fountain sound) in which case it is a script on the fountain object. Or the footstepController which must be on the player object that has the anim handler

---
+++ lAmP(Split3)/Assets/Scripts/Audio/MixerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixerController : MonoBehaviour
+{
+
+    public static MixerController instance;
+
+    // Make sure there is only 1 GameManager
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(transform);
+    }
+
+    [SerializeField]
+    private float defaultMasterVolume = 1.0f;
+    [SerializeField]
+    private float defaultMusicVolume = 1.0f;
+    [SerializeField]
+    private float defaultSFXVolume = 1.0f;
+
+    string masterBusString = "Bus:/";
+    string musicBusString = "Bus:/Music";
+    string sfxBusString = "Bus:/SFX";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FMOD.Studio.Bus masterBus;
+        masterBus = FMODUnity.RuntimeManager.GetBus(masterBusString);
+        masterBus.setVolume(defaultMasterVolume);
+
+        FMOD.Studio.Bus musicBus;
+        musicBus = FMODUnity.RuntimeManager.GetBus(musicBusString);
+        musicBus.setVolume(defaultMusicVolume);
+
+        FMOD.Studio.Bus sfxBus;
+        sfxBus = FMODUnity.RuntimeManager.GetBus(sfxBusString);
+        sfxBus.setVolume(defaultSFXVolume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetMasterVolume(float volume)
+    {
+        FMOD.Studio.Bus masterBus;
+        masterBus = FMODUnity.RuntimeManager.GetBus(masterBusString);
+        masterBus.setVolume(volume);
+    }
+    float GetMasterVolume()
+    {
+        FMOD.Studio.Bus masterBus;
+        masterBus = FMODUnity.RuntimeManager.GetBus(masterBusString);
+        float volume;
+        masterBus.getVolume(out volume);
+        return volume;
+    }
+
+    void SetMusicVolume(float volume)
+    {
+        FMOD.Studio.Bus musicBus;
+        musicBus = FMODUnity.RuntimeManager.GetBus(musicBusString);
+        musicBus.setVolume(volume);
+    }
+    float GetMusicVolume()
+    {
+        FMOD.Studio.Bus musicBus;
+        musicBus = FMODUnity.RuntimeManager.GetBus(musicBusString);
+        float volume;
+        musicBus.getVolume(out volume);
+        return volume;
+    }
+
+    void SetSFXVolume(float volume)
+    {
+        FMOD.Studio.Bus sfxBus;
+        sfxBus = FMODUnity.RuntimeManager.GetBus(sfxBusString);
+        sfxBus.setVolume(volume);
+    }
+    float GetSFXBus()
+    {
+        FMOD.Studio.Bus sfxBus;
+        sfxBus = FMODUnity.RuntimeManager.GetBus(sfxBusString);
+        float volume;
+        sfxBus.getVolume(out volume);
+        return volume;
+    }
+}
